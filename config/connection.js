ea4fab87a966197769e45d0623a4fdf669506414
aimontano <aimorales.montano@gmail.com>
2018-11-04 00:35:20 -0400

    Added the correct connection

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,19 @@
 const mysql = require('mysql');
-
-const connection = mysql.createConnection({
-	host: 'localhost',
-	port: 3306,
-	user: 'root',
-	password: '',
-	database: 'burgers_db'
-});
+const connection;
 
 if(process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL)
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-	connection.connect(err => {
-		if (err) throw err.stack;
-		console.log("Connected!!");
+	connection = mysql.createConnection({
+		host: 'localhost',
+		port: 3306,
+		user: 'root',
+		password: '',
+		database: 'burgers_db'
 	});
 }
-
+connection.connect(err => {
+	if (err) throw err.stack;
+	console.log("Connected!!");
+});
 module.exports = connection;
